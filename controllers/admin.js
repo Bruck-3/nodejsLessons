7242a3629159769f95eb874e3bf6919b32cb799e
bk-26 <49018192+bk-26@users.noreply.github.com>
2021-03-26 15:39:36 +0300

    ✨ edit Product feature for Admin

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,24 +1,50 @@
 const Product = require('../model/product');
 
 exports.getAddProduct = (req, res, next) => {
-  res.render('admin/add-product', {
+  res.render('admin/edit-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
-    formsCSS: true,
-    productCSS: true,
-    activeAddProduct: true
-  });
-};
+    edit: false
+});
+}
 
 exports.postAddProduct = (req, res, next) => {
   const title = req.body.title;
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(title, imageUrl, description, price);
+  const product = new Product(null,title, imageUrl, description, price);
   product.save();
   res.redirect('/');
 };
+
+exports.getEditProduct = (req , res, next) => {
+  const editMode = req.query.edit
+  console.log(editMode)
+  const productId = req.params.productId
+  if(!editMode){
+    return res.redirect('/')
+  }
+  Product.fetchById(productId, product =>{
+    res.render('admin/edit-product', {
+      prod: product,
+      path: '/admin/edit-product',
+      pageTitle: 'Edit Product',
+      edit: editMode
+    })
+  })
+}
+exports.postEditProduct = (req,res,next) =>{
+  const updatedId = req.body.productId
+  const updatedTitle = req.body.title;
+  const updatedImageUrl = req.body.imageUrl;
+  const updatedPrice = req.body.price;
+  const updatedDescription = req.body.description;
+  const updatedProduct = new Product(updatedId ,updatedTitle, updatedImageUrl, updatedDescription, updatedPrice);
+  updatedProduct.save();
+
+  res.redirect('/')
+}
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
@@ -28,4 +54,6 @@ exports.getProducts = (req, res, next) => {
       path: '/admin/products'
     });
   });
-};
+}
+
+
